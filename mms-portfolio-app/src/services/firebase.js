c6da211419cb0493cd89da551cdaf0a2333ed00d
mmswflow-upb <mohamad_mario.sakka@stud.fils.upb.ro>
2025-09-03 12:24:50 +0300

    moved firebase storage bucket to EU

--- mms-portfolio-app/src/services/firebase.js
+++ mms-portfolio-app/src/services/firebase.js
@@ -2,16 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
+import firebaseConfig from "../config/firebase-config.json";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyANdHMYmHXw2l0WV3sTRaM8puadeUesDaM",
-  authDomain: "mms-portfolio.firebaseapp.com",
-  projectId: "mms-portfolio",
-  storageBucket: "mms-portfolio.firebasestorage.app",
-  messagingSenderId: "496712903312",
-  appId: "1:496712903312:web:9f350ae3bae90ff7998615",
-  measurementId: "G-198RBQ33RP",
-};
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app, "mms-portfolio");
 export const storage = getStorage(app);
